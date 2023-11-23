0a0b7eda72dadb20850f3d7fa1c9527798775085
Paloma Wink <paloma.wink@hotmail.com>
2023-11-23 10:45:30 -0300

    Projeto iniciado

---
+++ newsletter/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Se mantenha atualizado</h1>
+      <p></p>
+    </div>
+  )
+}
+
+export default App
