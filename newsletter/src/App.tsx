22e7260f8eee616f6964612d8c763d46ad030e78
Paloma Wink <paloma.wink@hotmail.com>
2023-11-24 11:36:16 -0300

    Mais algumas alterações de CSS

--- newsletter/src/App.tsx
+++ newsletter/src/App.tsx
@@ -8,31 +8,31 @@ function App() {
   return (
     <div className='flex justify-center items-center h-screen'>
       <div className='grid grid-cols-4 max-w-3xl'>
-        <div className='col-span-2 flex flex-col justify-center space-y-7 m-7'>
-          <h1 className='text-5xl font-bold'>Stay updated!</h1>
+        <div className='col-span-2 flex flex-col justify-center space-y-11 m-7'>
+          <h1 className='text-5xl font-bold text-justify'>Stay updated!</h1>
           <p className='text-justify'>Join 60,000+ product managers receiving monthly updates on:</p>
           <div>
-            <div className='flex items-center space-x-2'>
+            <div className='flex items-center space-x-2 text-sm'>
               <img src={iconSuccess} alt='Imagem de check' />
-              <p>Product discovery and building what matters</p>
+              <p className='text-justify'>Product discovery and building what matters</p>
             </div>
-            <div className='flex items-center space-x-2'>
+            <div className='flex items-center space-x-2 text-sm'>
               <img src={iconSuccess} alt='Imagem de check' />
-              <p>Measuring to ensure updates are a success</p>
+              <p className='text-justify'>Measuring to ensure updates are a success</p>
             </div>
-            <div className='flex items-center space-x-2'>
+            <div className='flex items-center space-x-2 text-sm'>
               <img src={iconSuccess} alt='Imagem de check' />
-              <p>And much more</p>
+              <p className='text-justify'>And much more</p>
             </div>
           </div>
           <form>
             <p className='text-xs'>Email address</p>
-            <input type='text' className='border-solid border-2 border-black rounded-md h-8 w-64' />
+            <input type='text' className='border-solid border-2 border-black rounded-md h-10 w-72' />
             <button>Subscribe to monthly newsletter</button>
           </form>
         </div>
         <div className='col-span-2 flex items-center'>
-          <img src={illustration} alt='Imagem de fundo' className="h-96"/>
+          <img src={illustration} alt='Imagem de fundo' className="h-96 w-64"/>
         </div>
       </div>
     </div>
